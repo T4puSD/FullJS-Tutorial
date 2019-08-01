c7b3462bbdd7b91fa074f9eae5c89ad53676c92e
T4puSD <sdtapusd20@gmail.com>
2019-08-01 09:40:35 +0600

    added functionality to generate number form user input

--- script.js
+++ script.js
@@ -1,59 +1,98 @@
-oddlist = document.createElement("ul");
-evenlist = document.createElement("ul");
+//creating user input field
+let input = document.createElement("input");
+input.id = "input";
+input.type = "text";
 
-//generating odd numbers
-for (let i = 0; i <= 100; i++) {
-  if (i % 2 != 0) {
-    let li = document.createElement("li");
-    li.innerHTML = i;
-    oddlist.appendChild(li);
+let label = document.createElement("label");
+label.innerHTML = "Enter a Number:";
+
+label.setAttribute("for", "input");
+
+let submit = document.createElement("button");
+submit.type = "submit";
+submit.innerHTML = "Submit";
+submit.onclick = "generateList()";
+console.log(submit);
+
+submit.addEventListener("click", () => {
+  if (input.value) {
+    console.log(input.value);
+    generateList(input.value);
+  } else {
+    input.placeholder = "Please Enter number herer";
+    input.focus();
   }
-}
-
-//generating even numbers
-for (let i = 2; i <= 100; i++) {
-  if (i % 2 == 0) {
-    let li = document.createElement("li");
-    li.innerHTML = i;
-    evenlist.appendChild(li);
-  }
-}
-
-let main = document.createElement("main");
-let container = document.createElement("div");
-container.className = "container";
-
-let sub1 = document.createElement("div");
-sub1.className = "sub1";
-let sub2 = document.createElement("div");
-sub2.className = "sub2";
-
-//creating heade for each sub
-
-let sub1Header = document.createElement("h1");
-sub1Header.innerHTML = "Odd Numbers";
-let sub2Header = document.createElement("h1");
-sub2Header.innerHTML = "Even Numbers";
-
-sub1.append(sub1Header, oddlist);
-sub2.append(sub2Header, evenlist);
-
-container.append(sub1, sub2);
-
-main.appendChild(container);
-
-//getting body object from html
+  // generateList();
+});
+console.log(submit);
 
 let body = document.querySelector("body");
-body.append(main);
+body.append(label, input, submit);
+
+function generateList(value) {
+  let mm = document.querySelector("main");
+  if (mm) {
+    // console.log(mm);
+    body.removeChild(mm);
+  } else {
+    console.log("called for the first time");
+  }
+  // body.removeChild();
+  let oddlist = document.createElement("ul");
+  let evenlist = document.createElement("ul");
+  //generating odd numbers
+  for (let i = 0; i <= value; i++) {
+    if (i % 2 != 0) {
+      let li = document.createElement("li");
+      li.innerHTML = i;
+      oddlist.appendChild(li);
+    }
+  }
+
+  //generating even numbers
+  for (let i = 2; i <= value; i++) {
+    if (i % 2 == 0) {
+      let li = document.createElement("li");
+      li.innerHTML = i;
+      evenlist.appendChild(li);
+    }
+  }
+
+  let main = document.createElement("main");
+  let container = document.createElement("div");
+  container.className = "container";
+
+  let sub1 = document.createElement("div");
+  sub1.className = "sub1";
+  let sub2 = document.createElement("div");
+  sub2.className = "sub2";
+
+  //creating heade for each sub
+
+  let sub1Header = document.createElement("h1");
+  sub1Header.innerHTML = "Odd Numbers";
+  let sub2Header = document.createElement("h1");
+  sub2Header.innerHTML = "Even Numbers";
+
+  sub1.append(sub1Header, oddlist);
+  sub2.append(sub2Header, evenlist);
+
+  container.append(sub1, sub2);
+
+  main.appendChild(container);
+
+  //getting body object from html
+  body.append(main);
+}
 
 //creating css
 
 let style = document.createElement("style");
 style.innerHTML = `body{
     display:flex;
+    flex-direction:column;
+    align-items:center;
     justify-content:center;
-    align-item:center;
     }
     
     main{
@@ -85,8 +124,3 @@ style.innerHTML = `body{
 //finding head
 let head = document.querySelector("head");
 head.appendChild(style);
-
-console.log(main);
-
-console.log(oddlist);
-console.log(evenlist);
